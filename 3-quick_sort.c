7503754c927e5f8d4688ed2b0a9c23a58992ae97
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-22 19:05:13 +0100

    Added quick sort source file

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* quick_sort - Function that sorts array of integers
+* in ascending order using the quick sort algorithm
+*
+* @array: Array to be sorted
+* @size: Size of array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+
+}
