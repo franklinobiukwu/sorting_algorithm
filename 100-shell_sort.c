18da2e30839bd96069b4e2fbbe5f4b1c015f1c20
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-09-25 09:30:15 +0100

    Added shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* shell_sort - sort array of integers using the shell sort algorithm
+*
+* @array - array to be sorted
+* @size - size of array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	
+}
