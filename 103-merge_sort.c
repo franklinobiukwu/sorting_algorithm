c2a5134a579adeb5d36373acd887b8f4cd2cdc0d
MDIMACat <romanmdima@gmail.com>
2023-09-26 15:29:41 +0200

    Sorted betty issues

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -43,7 +43,6 @@ void merge_sort_recursion(int *array, int left, int right)
 * @right: Right indexed part of array
 * Return: Void
 */
-
 void merge_sorted_array(int *array, int left, int middle, int right)
 {
 	int left_length = middle - left + 1;
@@ -53,69 +52,33 @@ void merge_sorted_array(int *array, int left, int middle, int right)
 	int *temp_right = malloc(sizeof(int) * right_length);
 
 	if (temp_left == NULL || temp_right == NULL)
-	{
 		return;
-	}
 
 	printf("[left]: ");
 	for (i = 0; i < left_length; i++)
-	{
-		temp_left[i] = array[left + i];
-		printf("%d", temp_left[i]);
-		if (i < left_length - 1)
-			printf(", ");
-	}
-	printf("\n");
+		printf("%d%s", (temp_left[i] = array[left + i]), i < left_length
+		- 1 ? ", " : "\n");
 
 	printf("[right]: ");
 	for (j = 0; j < right_length; j++)
-	{
-		temp_right[j] = array[middle + 1 + j];
-		printf("%d", temp_right[j]);
-		if (j < right_length - 1)
-			printf(", ");
-	}
-	printf("\n");
+		printf("%d%s", (temp_right[j] = array[middle + 1 + j]),
+		j < right_length - 1 ? ", " : "\n");
 
-	i = 0;
-	j = 0;
+	i = j = 0;
 	k = left;
 	while (i < left_length && j < right_length)
-	{
-		if (temp_left[i] <= temp_right[j])
-		{
-			array[k] = temp_left[i];
-			i++;
-		}
-		else
-		{
-			array[k] = temp_right[j];
-			j++;
-		}
-		k++;
-	}
+		array[k++] = (temp_left[i] <= temp_right[j]) ?
+		temp_left[i++] : temp_right[j++];
 
 	while (i < left_length)
-	{
-		array[k] = temp_left[i];
-		i++;
-		k++;
-	}
+		array[k++] = temp_left[i++];
+
 	while (j < right_length)
-	{
-		array[k] = temp_right[j];
-		j++;
-		k++;
-	}
+		array[k++] = temp_right[j++];
 
 	printf("[Done]: ");
 	for (i = left; i <= right; i++)
-	{
-		printf("%d", array[i]);
-		if (i < right)
-			printf(", ");
-	}
-	printf("\n");
+		printf("%d%s", array[i], i < right ? ", " : "\n");
 
 	free(temp_left);
 	free(temp_right);
